d195ddf51835eced407d1393a13a43c19f1b023f
KRUNCH7SHooT <65598325+KRUNCHSHooT@users.noreply.github.com>
2021-06-15 08:14:02 +0700

    Create Fortune.php

---
+++ src/KRUNCHSHooT/Fortune/Fortune.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KRUNCHSHooT\Fortune;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\item\enchantment\Enchantment;
+
+class Fortune extends PluginBase {
+
+		public static $inDisableWorlds;
+
+	
+
+	public function onEnable(){
+
+		$this->getLogger()->info("Fortune Plugin has been successfully registered");
+
+		$this->getServer()->getPluginManager()->registerEvents(new FortuneListener($this), $this);
+
+		self::$inDisableWorlds = (array) $this->getConfig()->get("disableworlds");
+
+	}
+
+	
+
+	public function onLoad(){
+
+		$this->getLogger()->info("currently registering enchantment fortune...");
+
+		Enchantment::registerEnchantment(new Enchantment(Enchantment::FORTUNE, "Fortune", Enchantment::RARITY_UNCOMMON, Enchantment::SLOT_DIG, Enchantment::SLOT_HOE, 3));
+
+		
+
+		if(!file_exists($this->getDataFolder())){
+
+			@mkdir($this->getDataFolder());
+
+			$this->saveResource("config.yml");
+
+		}
+
+	}
+
+	
+
+	public function disableWorlds(string $world) : bool {
+
+		return in_array($world, self::$inDisableWorlds);
+
+	}
+
+}
