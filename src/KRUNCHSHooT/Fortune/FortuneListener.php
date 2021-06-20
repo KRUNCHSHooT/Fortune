e074ea5cf7743988f51849575f35d9b073321964
KRUNCH7SHooT <65598325+KRUNCHSHooT@users.noreply.github.com>
2021-06-20 14:32:35 +0700

    update FortuneListener.php

--- src/KRUNCHSHooT/Fortune/FortuneListener.php
+++ src/KRUNCHSHooT/Fortune/FortuneListener.php
@@ -316,7 +316,69 @@ class FortuneListener implements Listener {
 							$event->setDrops([Item::get(Item::FLINT)]);
 						}
 					break;
-                                        //TODO: GlowStone
+					case Block::GLOWSTONE:
+					    if($item->getEnchantmentLevel(Enchantment::FORTUNE) == 1){
+					        if($chance >= 1 && $chance <= 25){
+					            $add = 1;
+					        } elseif($chance >= 26 && $chance <= 44){
+					            $add = 2;
+					        } elseif($chance >= 45 && $chance <= 63){
+					            $add = 3;
+					        } elseif($chance >= 64 && $chance <= 82){
+					            $add = 4;
+					        } elseif($chance >= 83 && $chance <= 100){
+					            $add = 4;
+					        }
+					    } elseif($item->getEnchantmentLevel(Enchantment::FORTUNE) == 2){
+					        if($chance >= 1 && $chance <= 20){
+					            $add = 1;
+					        } elseif($chance >= 21 && $chance <= 36){
+					            $add = 2;
+					        } elseif($chance >= 37 && $chance <= 52){
+					            $add = 3;
+					        } elseif($chance >= 53 && $chance <= 68){
+					            $add = 4;
+					        } elseif($chance >= 69 && $chance <= 84){
+					            $add = 4;
+					        } elseif($chance >= 84 && $chance <= 100){
+					            $add = 4;
+					        }
+					    } elseif($item->getEnchantmentLevel(Enchantment::FORTUNE) == 3){
+					        if($chance >= 1 && $chance <= 17){
+					            $add = 1;
+					        } elseif($chance >= 18 && $chance <= 31){
+					            $add = 2;
+					        } elseif($chance >= 32 && $chance <= 45){
+					            $add = 3;
+					        } elseif($chance >= 46 && $chance <= 59){
+					            $add = 4;
+					        } elseif($chance >= 60 && $chance <= 73){
+					            $add = 4;
+					        } elseif($chance >= 74 && $chance <= 87){
+					            $add = 4;
+					        } elseif($chance >= 88 && $chance <= 100){
+					            $add = 4;
+					        }
+					    } else {
+					        $lvl = $item->getEnchantmentLevel(Enchantment::FORTUNE);
+					        if($chance >= 1 && $chance <= 17){
+					            $add = 1;
+					        } elseif($chance >= 18 && $chance <= 31){
+					            $add = round($lvl / 4) + 2;
+					        } elseif($chance >= 32 && $chance <= 45){
+					            $add = round($lvl / 4) + 3;
+					        } elseif($chance >= 46 && $chance <= 59){
+					            $add = round($lvl / 4) + 4;
+					        } elseif($chance >= 60 && $chance <= 73){
+					            $add = round($lvl / 4) + 4;
+					        } elseif($chance >= 74 && $chance <= 87){
+					            $add = round($lvl / 4) + 4;
+					        } elseif($chance >= 88 && $chance <= 100){
+					            $add = round($lvl / 4) + 4;
+					        }
+					    }
+					    $event->setDrops([Item::get(Item::GLOWSTONE_DUST, 0, rand(2, 4) * $add)]);
+					break;
                                         //TODO: Melon
                                         //TODO: Sea Lantern
 				}
