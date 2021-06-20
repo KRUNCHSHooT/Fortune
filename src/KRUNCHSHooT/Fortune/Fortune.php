76570aa2f6ebbc8285d91635b0d2ce698e2d01cc
KRUNCH7SHooT <65598325+KRUNCHSHooT@users.noreply.github.com>
2021-06-20 14:47:06 +0700

    update Fortune.php

--- src/KRUNCHSHooT/Fortune/Fortune.php
+++ src/KRUNCHSHooT/Fortune/Fortune.php
@@ -11,14 +11,11 @@ class Fortune extends PluginBase {
 	public static $inDisableWorlds;
 	
 	public function onEnable(){
-		$this->getLogger()->info("Fortune Plugin has been successfully registered");
 		$this->getServer()->getPluginManager()->registerEvents(new FortuneListener($this), $this);
 		self::$inDisableWorlds = (array) $this->getConfig()->get("disableworlds");
 	}
 	
 	public function onLoad(){
-		$this->getLogger()->info("currently registering enchantment fortune...");
-
 		Enchantment::registerEnchantment(new Enchantment(Enchantment::FORTUNE, "Fortune", Enchantment::RARITY_UNCOMMON, Enchantment::SLOT_DIG, Enchantment::SLOT_HOE, 3));
 		
 		if(!file_exists($this->getDataFolder())){
